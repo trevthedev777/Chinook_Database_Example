36750110dcadfafb08bb4b5b0f5f9791c44dd033
trevthedev777 <bloodsportrecords@gmail.com>
2022-10-21 03:06:11 +0000

    run 6 basic search quieries using sql alchemy and update the readme

--- sql-expression.py
+++ sql-expression.py
@@ -47,7 +47,25 @@ track_table = Table(
 with db.connect() as connection:
     
     # Query 1 - select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
+
+    # Query 2 -  select only the "Name" column from the "Artist" Table
+    # using .c will allow us to search for a specifil column on the table
+    # to select only the specific artist name use the .with_only_columns([specify this column]) # noqa
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name]) # noqa
+
+    # Query 3 - select only 'Queen' from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen") # noqa
+
+    # Query 4 - Select only the "ArtistId" #51 from the "Artist" table
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    # Query 5  - select only the albums with "ArtistId" #51 on the "Album" Table # noqa
+    # select_query = artist_table.select().where(album_table.c.ArtistId == 51)
+
+    # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
+    
 
     results = connection.execute(select_query)
     for result in results:
