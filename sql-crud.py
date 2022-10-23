45764c5914609b837bff68e642bdd4f08976c5b0
trevthedev777 <bloodsportrecords@gmail.com>
2022-10-23 18:58:24 +0000

    create new table in chinook db, create record and test instance and compilation of db

--- sql-crud.py
+++ sql-crud.py
@@ -9,6 +9,18 @@ from sqlalchemy.orm import sessionmaker
 db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
+
+# Create a class-based model for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+    
+
 # Three things we need to do to access and query our db
 # =====================================================
 # instead of connecting to the databse directly, we will ask for a session
@@ -19,3 +31,59 @@ session = Session()
 
 # creating the databse using declarative_base subclass
 base.metadata.create_all(db)
+
+
+# creating records on our programmer table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer"
+)
+
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL Language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+
+# Add each instance of our proigrammer to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+
+# commit our session to the database
+session.commit()
+
+# query the database to find all the programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + "  " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep=" | "
+    )
